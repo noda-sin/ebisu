636c2ad1c860243e90b26a25ee67b79b3c244ebb
Noda Shimpei <noda.sin@gmail.com>
2018-03-25 19:31:04 +0900

    Digest auth

--- app.py
+++ app.py
@@ -2,13 +2,24 @@ import argparse
 import json
 
 from flask import Flask, render_template, url_for, request
+from flask_httpauth import HTTPDigestAuth
 
 from bot import Bot, os
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('DEEPMEX_SECRET')
+auth = HTTPDigestAuth()
+
 bot = Bot()
 
+@auth.get_password
+def get_pw(username):
+    if username != os.environ.get('DEEPMEX_USERNAME'):
+        return None
+    return os.environ.get('DEEPMEX_PASSWORD')
+
 @app.route('/')
+@auth.login_required
 def index():
     market_price = bot.market_last_price()
     position     = bot.bitmex.current_position()
