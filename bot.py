6cf0efefe860102f75938b481efacee821e6217b
Noda Shimpei <noda.sin@gmail.com>
2018-04-10 00:33:34 +0900

    ドテンくんのベースを作成

--- bot.py
+++ bot.py
@@ -5,17 +5,29 @@ import sys
 import threading
 import time
 from datetime import datetime, timedelta
+import numpy as np
+import pandas as pd
 import bitmex
 
+def highest(source, period):
+    return pd.rolling_max(source, period, 1)
+
+def lowest(source, period):
+    return pd.rolling_min(source, period, 1)
+
+def stdev(source, period):
+    return pd.rolling_std(source, period, 1)
+
+def sma(source, period):
+    return pd.rolling_mean(source, period, 1)
+
 class BitMex:
+    listener = None
+
     def __init__(self, test=True):
         apiKey = os.environ.get('BITMEX_APIKEY') if test else os.environ.get('BITMEX_TEST_APIKEY')
         secret = os.environ.get('BITMEX_SECRET') if test else os.environ.get('BITMEX_TEST_SECRET')
         self.client = bitmex.bitmex(test=test, api_key=apiKey, api_secret=secret)
-
-    def has_open_orders(self):
-        orders = self.client.Order.Order_getOrders(symbol='XBTUSD', filter=json.dumps({'open': True})).result()[0][0]
-        return len(orders) > 0
 
     def current_position(self):
         return self.client.Position.Position_get(filter=json.dumps({'symbol': 'XBTUSD'})).result()[0][0]['currentQty']
@@ -27,13 +39,13 @@ class BitMex:
     def entry(self, side, size):
         order = self.client.Order.Order_new(symbol='XBTUSD', ordType='Market',
                                     side=side.capitalize(), orderQty=size).result()[0]
-        print(str(time) + ' Create Order: ' + order['ordType'] + ' ' + order['side'] + ': ' +
+        print(str(datetime.now()) + ' Create Order: ' + order['ordType'] + ' ' + order['side'] + ': ' +
               str(order['orderQty']) + ' @ ' + str(order['price']) + ' / ' + order['orderID'])
 
     def cancel_orders(self):
         orders = self.client.Order.Order_cancelAll().result()[0]
         for order in orders:
-            print(str(time) + ' Cancel Order: ' + order['ordType'] + ' ' + order['side'] + ': ' +
+            print(str(datetime.now()) + ' Cancel Order: ' + order['ordType'] + ' ' + order['side'] + ': ' +
                   str(order['orderQty']) + ' @ ' + str(order['price']))
 
     def fetch_ohlc(self, starttime=(datetime.now() + timedelta(days=-30)), endTime=datetime.now()):
@@ -41,46 +53,166 @@ class BitMex:
                                                       startTime=starttime, endTime=endTime).result()[0]
         return candles[:-1]
 
-class Bot:
-    def __init__(self, lot=100, test=True):
-        self.originallot = lot
-        self.bitmex = BitMex(test=test)
-
-    def _run(self):
+    def _crawler_run(self):
         dt_prev = datetime.now()
         while True:
             dt_now = datetime.now()
             if dt_now - dt_prev < timedelta(hours=1):
                 continue
             try:
-                source = self.bitmex.fetch_ohlc()
-                open  = [v[1] for _, v in enumerate(source)]
-                high  = [v[2] for _, v in enumerate(source)]
-                low   = [v[3] for _, v in enumerate(source)]
-                close = [v[4] for _, v in enumerate(source)]
-                up_bound = max(high)
-                dn_bound = min(low)
-
-                pos = self.bitmex.current_position()
-                lot = self.originallot
-                if high[-1] == up_bound and pos <= 0:
-                    if pos < 0:
-                        lot = 2 * self.originallot
-                    self.bitmex.entry(side='long', size=lot)
-                elif low[-1] == dn_bound and pos >= 0:
-                    if pos > 0:
-                        lot = 2 * self.originallot
-                    self.bitmex.entry(side='short', size=lot)
+                source = self.fetch_ohlc()
+                if self.listener is not None:
+                    self.listener(source)
             except Exception as e:
                 print(e)
+                continue
             dt_prev = dt_now
+
+    def on_update(self, listener):
+        self.listener = listener
+        crawler = threading.Thread(target=self._crawler_run)
+        crawler.start()
+
+class BitMexStub(BitMex):
+    balance      = 200
+    current_qty  = 0
+    entry_price  = 0
+    market_price = 0
+
+    now_index    = None
+    now_time     = None
+
+    buy_signals  = []
+    sell_signals = []
+    balance_history = []
+
+    def __init__(self):
+        BitMex.__init__(self)
+        self.load_ohlc()
+
+    def current_position(self):
+        return self.current_qty
+
+    def entry(self, side, size):
+        if side == 'long':
+            self.buy_signals.append(self.now_index)
+            order_qty = size
+        else:
+            self.sell_signals.append(self.now_index)
+            order_qty = -size
+
+        next_qty = self.current_qty + order_qty
+
+        if (self.current_qty > 0 and order_qty <= 0) or \
+                (self.current_qty < 0 and order_qty > 0):
+            profit = ((self.entry_price - self.market_price) / self.entry_price - 0.075) * self.current_qty
+            self.balance += profit
+            print(str(self.now_time) + ' Closed Position @ ' + str(self.balance))
+
+        if next_qty != 0:
+            print(str(self.now_time) + ' Create Order: ' + str(next_qty) + ' / ' + str(self.market_price) + ' @ ' + str(self.balance))
+            self.current_qty = next_qty
+            self.entry_price = self.market_price
+
+    def load_ohlc(self):
+        if os.path.exists('ohlc.csv'):
+            self.ohlc_df = pd.read_csv('ohlc.csv')
+            return
+
+        starttime = datetime(year=2018, month=1, day=1, hour=0, minute=0)
+        endtime   = datetime(year=2018, month=4, day=1, hour=0, minute=0)
+
+        lefttime  = starttime
+        righttime = starttime + 99 * timedelta(hours=1)
+
+        list = []
+        while True:
+            source = BitMex.fetch_ohlc(self, starttime=lefttime, endTime=righttime)
+            list.extend(source)
+
+            lefttime  = lefttime  + 100 * timedelta(hours=1)
+            righttime = righttime + 100 * timedelta(hours=1)
+
+            if lefttime < endtime and righttime > endtime:
+                righttime = endtime
+                continue
+            elif lefttime > endtime:
+                break
+
+        self.ohlc_df = pd.DataFrame(list)
+        self.ohlc_df.to_csv('ohlc.csv')
+
+    def _crawler_run(self):
+        source = []
+        for index, row in self.ohlc_df.iterrows():
+            source.append((row[9], row[7], row[1], row[3], row[6]))
+            self.market_price = row[1]
+            self.now_index    = index
+            self.now_time     = row[9]
+            if len(source) > 20:
+                self.listener(source)
+                source.pop(0)
+            self.balance_history.append(self.balance-200)
+
+    def on_update(self, listener):
+        self.listener = listener
+        self._crawler_run()
+
+    def plot_result(self):
+        import matplotlib.pyplot as plt
+        plt.figure()
+        plt.subplot(211)
+        plt.plot(self.ohlc_df.index, self.ohlc_df["high"])
+        plt.plot(self.ohlc_df.index, self.ohlc_df["low"])
+        plt.ylabel("Price(USD)")
+        ymin = min(self.ohlc_df["low"]) - 200
+        ymax = max(self.ohlc_df["high"]) + 200
+        plt.vlines(self.buy_signals,  ymin, ymax, "blue", linestyles='dashed', linewidth=1)
+        plt.vlines(self.sell_signals, ymin, ymax, "red",  linestyles='dashed', linewidth=1)
+        # plt.vlines(buyCloseSignals, ymin, ymax, "black", linestyles='dashed', linewidth=1)
+        # plt.vlines(sellCloseSignals, ymin, ymax, "green", linestyles='dashed', linewidth=1)
+        plt.subplot(212)
+        plt.plot(self.ohlc_df.index, self.balance_history)
+        plt.hlines(y=0, xmin=self.ohlc_df.index[0], xmax=self.ohlc_df.index[-1], colors='k', linestyles='dashed')
+        plt.ylabel("PL(USD)")
+        plt.show()
+
+class Bot:
+    def __init__(self, lot=100, test=True):
+        self.originallot = lot
+        self.is_test = test
+        if test:
+            self.bitmex = BitMexStub()
+        else:
+            self.bitmex = BitMex()
+
+    def strategy(self, source):
+        open  = np.array([v[1] for _, v in enumerate(source)])
+        high  = np.array([v[2] for _, v in enumerate(source)])
+        low   = np.array([v[3] for _, v in enumerate(source)])
+        close = np.array([v[4] for _, v in enumerate(source)])
+
+        is_up = high[-1] == highest(high, 18)[-1]
+        is_dn = low[-1]  == lowest(low, 18)[-1]
+
+        pos = self.bitmex.current_position()
+        lot = self.originallot
+        if is_up and pos <= 0:
+            if pos < 0:
+                lot = 2 * self.originallot
+            self.bitmex.entry(side='long', size=lot)
+        elif is_dn and pos >= 0:
+            if pos > 0:
+                lot = 2 * self.originallot
+            self.bitmex.entry(side='short', size=lot)
 
     def run(self):
         try:
-            runner = threading.Thread(target=self._run)
-            runner.daemon = True
-            runner.start()
-            while True: time.sleep(100)
+            self.bitmex.on_update(listener=self.strategy)
+            if self.is_test:
+                self.bitmex.plot_result()
+            else:
+                while True: time.sleep(100)
         except (KeyboardInterrupt, SystemExit):
             self.exit()
 
