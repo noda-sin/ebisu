31e1ec6720d97546c45a4a2459279c418398f7fd
Noda Shimpei <noda.sin@gmail.com>
2018-04-15 11:29:34 +0900

    バックテスト調整

--- bot.py
+++ bot.py
@@ -21,9 +21,10 @@ class Side:
 
 DEFAULT_BL   = 999999
 DEFAULT_LOT  = 100
-DEFAULT_LEVA = 3
+DEFAULT_LEVA = 1
 
-OHLC_FILENAME = os.path.join(os.path.dirname(__file__), "ohlc_{}_{}.csv")
+OHLC_DIRNAME  = os.path.join(os.path.dirname(__file__), "ohlc/{}")
+OHLC_FILENAME = os.path.join(os.path.dirname(__file__), "ohlc/{}/ohlc_{}.csv")
 
 def highest(source, period):
     return pd.rolling_max(source, period, 1)
@@ -47,6 +48,12 @@ def delta(tr='1h'):
     else:
         return timedelta(hours=1)
 
+def change_rate(a, b):
+    if a > b:
+        return a/b
+    else:
+        return b/a
+
 class BitMex:
     listener = None
 
@@ -58,6 +65,9 @@ class BitMex:
 
     def current_position(self):
         return self.client.Position.Position_get(filter=json.dumps({'symbol': 'XBTUSD'})).result()[0][0]['currentQty']
+
+    def current_entryprice(self):
+        return self.client.Position.Position_get(filter=json.dumps({'symbol': 'XBTUSD'})).result()[0][0]['avgEntryPrice']
 
     def close_position(self, time=datetime.now()):
         self.client.Order.Order_closePosition(symbol='XBTUSD').result()
@@ -81,7 +91,7 @@ class BitMex:
     def fetch_ohlc(self, starttime=(datetime.now() + timedelta(days=-30)), endTime=datetime.now()):
         candles = self.client.Trade.Trade_getBucketed(symbol='XBTUSD', binSize=self.tr,
                                                       startTime=starttime, endTime=endTime).result()[0]
-        return candles[:-1]
+        return candles
 
     def _crawler_run(self):
         dt_prev = datetime.now()
@@ -112,6 +122,15 @@ class BitMexStub(BitMex):
     ohlc_df      = None
     now_index    = None
     now_time     = None
+    order_count  = 0
+
+    win_count    = 0
+    lose_count   = 0
+
+    win_profit   = 0
+    lose_loss    = 0
+
+    max_drowdown = 0
 
     buy_signals  = []
     sell_signals = []
@@ -127,6 +146,16 @@ class BitMexStub(BitMex):
     def current_position(self):
         return self.current_qty
 
+    def current_entryprice(self):
+        return self.entry_price
+
+    def close_position(self, time=datetime.now()):
+        pos = self.current_qty
+        if pos > 0:
+            self.entry(side=Side.Short, size=pos)
+        elif pos < 0:
+            self.entry(side=Side.Long, size=-1*pos)
+
     def entry(self, side, size):
         if side == Side.Long:
             self.buy_signals.append(self.now_index)
@@ -139,14 +168,66 @@ class BitMexStub(BitMex):
 
         if (self.current_qty > 0 and order_qty <= 0) or \
                 (self.current_qty < 0 and order_qty > 0):
-            profit = ((self.entry_price - self.market_price) / self.entry_price - self.trade_fee()) * self.current_qty * self.current_leverage()
+
+            if self.entry_price > self.market_price:
+                close_rate = (self.entry_price - self.market_price) / self.market_price - self.trade_fee()
+                profit = -1 * self.current_qty * self.current_qty * close_rate
+            else:
+                close_rate = (self.market_price - self.entry_price) / self.entry_price - self.trade_fee()
+                profit = self.current_qty * self.current_qty * close_rate
+
+            if profit > 0:
+                self.win_profit += profit
+                self.win_count  += 1
+            else:
+                self.lose_loss  += -1 * profit
+                self.lose_count += 1
+                if close_rate > self.max_drowdown:
+                    self.max_drowdown = close_rate
+
             self.balance += profit
-            print(str(self.now_time) + ' Closed Position @ ' + str(self.balance))
+            print('{} # Close Position @ {} {} {} {}'.format(self.order_count, side, self.now_time, self.market_price, profit))
 
         if next_qty != 0:
-            print(str(self.now_time) + ' Create Order: ' + str(next_qty) + ' / ' + str(self.market_price) + ' @ ' + str(self.balance))
+            print('{} # Create Order   @ {} {} {}'.format(self.order_count, side, self.now_time, self.market_price))
+
             self.current_qty = next_qty
             self.entry_price = self.market_price
+
+        self.order_count+=1
+
+    def clean_ohlc(self):
+        source = []
+        for index, row in self.ohlc_df.iterrows():
+            if len(source) == 0:
+                source.append({
+                    'timestamp': row['timestamp'][:-6],
+                    'open': row['open'],
+                    'close': row['close'],
+                    'high': row['high'],
+                    'low': row['low']
+                })
+                continue
+
+            prev_row = source[-1]
+
+            timestamp = row['timestamp'][:-6]
+            open      = prev_row['open'] if change_rate(prev_row['open'], row['open']) > 1.5 else row['open']
+            close     = prev_row['close'] if change_rate(prev_row['close'], row['close']) > 1.5 else row['close']
+            high      = prev_row['high'] if change_rate(prev_row['high'], row['high']) > 1.5 else row['high']
+            low       = row['low']
+
+            source.append({'timestamp': timestamp, 'open': open, 'close': close, 'high': high, 'low': low})
+
+        source = pd.DataFrame(source)
+        self.ohlc_df = pd.DataFrame({
+            'timestamp': pd.to_datetime(source['timestamp']),
+            'open':  source['open'],
+            'close': source['close'],
+            'high':  source['high'],
+            'low':   source['low']
+        })
+        self.ohlc_df.index = self.ohlc_df['timestamp']
 
     def load_ohlc(self):
         i = 0
@@ -154,15 +235,16 @@ class BitMexStub(BitMex):
             while True:
                 filename = OHLC_FILENAME.format(self.tr, i)
                 if os.path.exists(filename) and self.ohlc_df is None:
-                    print('Load ' + filename)
                     self.ohlc_df = pd.read_csv(filename)
                     i += 1
                 elif os.path.exists(filename):
-                    print('Load ' + filename)
                     self.ohlc_df = pd.concat([self.ohlc_df, pd.read_csv(filename)], ignore_index=True)
                     i += 1
                 else:
+                    self.clean_ohlc()
                     return
+
+        os.makedirs(OHLC_DIRNAME.format(self.tr))
 
         starttime = datetime(year=2017, month=1, day=1, hour=0, minute=0)
         endtime   = datetime(year=2018, month=4, day=1, hour=0, minute=0)
@@ -172,8 +254,6 @@ class BitMexStub(BitMex):
 
         list = []
         while True:
-            print('Fetch ohlc ' + str(lefttime) + ' ... ' + str(righttime))
-
             try:
                 source = BitMex.fetch_ohlc(self, starttime=lefttime, endTime=righttime)
             except Exception as e:
@@ -190,7 +270,6 @@ class BitMexStub(BitMex):
                 righttime = endtime
             elif lefttime > endtime:
                 df = pd.DataFrame(list)
-                print('Save ohlc ' + OHLC_FILENAME.format(self.tr, i))
                 df.to_csv(OHLC_FILENAME.format(self.tr, i))
                 break
 
@@ -198,7 +277,6 @@ class BitMexStub(BitMex):
 
             if len(list) > 65000:
                 df = pd.DataFrame(list)
-                print('Save ohlc ' + OHLC_FILENAME.format(self.tr, i))
                 df.to_csv(OHLC_FILENAME.format(self.tr, i))
                 list = []
                 i += 1
@@ -208,10 +286,10 @@ class BitMexStub(BitMex):
     def _crawler_run(self):
         source = []
         for index, row in self.ohlc_df.iterrows():
-            source.append((row[9], row[7], row[1], row[3], row[6]))
-            self.market_price = row[1]
+            source.append((row['timestamp'], row['open'], row['close'], row['high'], row['low']))
+            self.market_price = row['open']
+            self.now_time     = row['timestamp'] + timedelta(hours=9)
             self.now_index    = index
-            self.now_time     = row[9]
             if len(source) > 20:
                 self.listener(source)
                 source.pop(0)
@@ -221,7 +299,14 @@ class BitMexStub(BitMex):
         self.listener = listener
         self._crawler_run()
 
-    def plot_result(self):
+    def result(self):
+        print('#--------------------------------------------------------')
+        print('# トレード回数 : {}'.format(self.order_count))
+        print('# 勝率 : {} %'.format((self.win_count)/(self.win_count+self.lose_count)*100))
+        print('# プロフィッットファクター : {}'.format((self.win_profit)/(self.lose_loss)))
+        print('# 最大ドローダウン : {}'.format((self.max_drowdown) * 100))
+        print('#--------------------------------------------------------')
+
         import matplotlib.pyplot as plt
         plt.figure()
         plt.subplot(211)
@@ -246,18 +331,20 @@ class Bot:
             self.bitmex = BitMexStub()
         else:
             self.bitmex = BitMex()
-
+            
     def strategy(self, source):
-        open  = np.array([v[Source.Open]  for _, v in enumerate(source)])
-        high  = np.array([v[Source.High]  for _, v in enumerate(source)])
-        low   = np.array([v[Source.Low]   for _, v in enumerate(source)])
-        close = np.array([v[Source.Close] for _, v in enumerate(source)])
+        open  = np.array([v[Source.Open]  for _, v in enumerate(source[:-1])])
+        high  = np.array([v[Source.High]  for _, v in enumerate(source[:-1])])
+        low   = np.array([v[Source.Low]   for _, v in enumerate(source[:-1])])
+        close = np.array([v[Source.Close] for _, v in enumerate(source[:-1])])
 
         is_up = high[-1] == highest(high, 18)[-1]
-        is_dn = low[-1]  == lowest(low, 18)[-1]
+        is_dn = low[-1] == lowest(low, 18)[-1]
 
         pos = self.bitmex.current_position()
         lot = self.originallot
+
+        # エントリー
         if is_up and pos <= 0:
             if pos < 0:
                 lot = 2 * self.originallot
@@ -271,7 +358,7 @@ class Bot:
         try:
             self.bitmex.on_update(listener=self.strategy)
             if self.is_test:
-                self.bitmex.plot_result()
+                self.bitmex.result()
             else:
                 while True: time.sleep(100)
         except (KeyboardInterrupt, SystemExit):
