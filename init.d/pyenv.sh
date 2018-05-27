ed4574682e3c2b2f7c499e98bb94c4a340d0a330
noda shimpei <noda.sin@gmail.com>
2018-05-27 16:46:21 +0900

    環境ファイルの読み込み

--- init.d/pyenv.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-export PYENV_ROOT="/root/.pyenv"
-export PATH="${PYENV_ROOT}/shims:${PYENV_ROOT}/bin:${PATH}"
--- restart.sh
+++ restart.sh
@@ -5,9 +5,7 @@ cd /usr/local/bin/daikokuten
 
 # env
 rm -f /etc/init.d/daikokuten
-rm -f /etc/init.d/pyenv.sh
 ln -s /usr/local/bin/daikokuten/init.d/daikokuten /etc/init.d/daikokuten
-ln -s /usr/local/bin/daikokuten/init.d/pyenv.sh /etc/profile.d/pyenv.sh
 
 # force pull
 git fetch origin
