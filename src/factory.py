c3fb74a4eeb89b530957154604268b8f983a94b1
Noda Shimpei <noda.sin@gmail.com>
2018-06-05 13:05:42 +0900

    パラメータ引数を除去

--- src/factory.py
+++ src/factory.py
@@ -6,14 +6,13 @@ import src.strategy as strategy
 class BotFactory():
 
     @staticmethod
-    def create(name, demo=False, stub=False, test=False, params=None):
+    def create(name, demo=False, stub=False, test=False):
         """
         Botを作成する関数。名前から該当のBotを src/strategy.py から探す。
         :param name: Bot名
         :param demo: テストネット環境を利用するか
         :param stub: スタブ取引か
         :param test: バックテストか
-        :param params: 戦略のパラメータ
         :return: Bot
         """
         try:
@@ -22,7 +21,6 @@ class BotFactory():
             bot.test_net = demo
             bot.back_test = test
             bot.stub_test = stub
-            bot.params = params
             return bot
         except Exception as _:
             raise Exception(f"Not Found Strategy : {name}")
