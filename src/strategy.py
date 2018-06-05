54d7458e7f3baf9047699dcdf440c9442651f775
Noda Shimpei <noda.sin@gmail.com>
2018-06-05 12:56:27 +0900

    VixRci戦略を実装

--- src/strategy.py
+++ src/strategy.py
@@ -66,6 +66,91 @@ class Rci(Bot):
         elif close_all:
             self.exchange.close_all()
 
+# VixRci戦略
+class VixRci(Bot):
+    def __init__(self):
+        Bot.__init__(self, '5m')
+
+    def strategy(self, open, close, high, low):
+        lot = self.exchange.get_lot()
+        pos = self.exchange.get_position_size()
+
+        pd   = 24
+        bbl  = 20
+        mult = 1.9
+        lb   = 88
+        ph   = 0.85
+        pl   = 1.01
+
+        rci_limit = 20
+        rci_diff  = 30
+
+        itvs = 9
+        itvm = 36
+        itvl = 55
+
+        wvf        = ((highest(close, pd) - low) / (highest(close, pd))) * 100
+        s_dev      = mult * stdev(wvf, bbl)
+        mid_line   = sma(wvf, bbl)
+        lower_band = mid_line - s_dev
+        upper_band = mid_line + s_dev
+
+        range_high = (highest(wvf, lb)) * ph
+        range_low  = (lowest(wvf, lb)) * pl
+
+        green_hist = [wvf[-i] >= upper_band[-i] or wvf[-i] >= range_high[-i] for i in range(8)][::-1]
+        red_hist   = [wvf[-i] <= lower_band[-i] or wvf[-i] <= range_low[-i]  for i in range(8)][::-1]
+
+        # VIX Color Change
+        up1 = [(not green_hist[-i]) and green_hist[-i-1] and green_hist[-i-2]
+               and (not green_hist[-i-3]) and (not green_hist[-i-4]) for i in range(len(green_hist)-5)][::-1]
+        dn1 = [(not red_hist[-i]) and red_hist[-i-1] and red_hist[-i-2]
+               and (not red_hist[-i-3]) and (not red_hist[-i-4])     for i in range(len(red_hist)-5)][::-1]
+
+        dvup = red_hist[-1]   and red_hist[-2]
+        dvdn = green_hist[-1] and green_hist[-2]
+
+        # RCI
+        rci_short_arr  = rci(close, itvs)
+        rci_middle_arr = rci(close, itvm)
+        rci_long_arr   = rci(close, itvl)
+
+        rci_short  = rci_short_arr[-1]
+        rci_middle = rci_middle_arr[-1]
+        rci_long   = rci_long_arr[-1]
+
+        up2 = rci_short < 0
+        dn2 = rci_short > 0
+
+        up31 = rci_long < 0 and rci_middle < 0 and crossover(rci_middle_arr, rci_long_arr)
+        up32 = rci_long < -100 + rci_limit and rci_middle < -100 + rci_limit
+        up33 = rci_long < 0 and rci_middle < 0 and rci_middle > rci_long
+        up34 = rci_long < 0 and rci_middle < 0 and rci_long > rci_middle and rci_long - rci_middle < rci_diff
+
+        up3 = up31 or up32 or up33 or up34
+
+        dn31 = rci_long > 0 and rci_middle > 0 and crossunder(rci_middle_arr, rci_long_arr)
+        dn32 = rci_long > 100 - rci_limit and rci_middle > 100 - rci_limit
+        dn33 = rci_long > 0 and rci_middle > 0 and rci_middle < rci_long
+        dn34 = rci_long > 0 and rci_middle > 0 and rci_long < rci_middle and rci_middle - rci_long < rci_diff
+
+        dn3 = dn31 or dn32 or dn33 or dn34
+
+        long1  = (up1[-1] or up1[-2] or up1[-3]) and (up2 or up3)
+        long2  = dvup and up2 and up3
+        short1 = (dn1[-1] or dn1[-2] or dn1[-3]) and (dn2 or dn3)
+        short2 = dvdn and dn2 and dn3
+
+        exitlong  = (pos > 0 and (rci_middle > 70  or (short1 or short2)))
+        exitshort = (pos < 0 and (rci_middle < -70 or (long1 or long2)))
+
+        if (long1 or long2) and not (short1 or short2 or exitlong):
+            self.exchange.entry("Long", True, lot)
+        elif (short1 or short2) and not (long1 or long2 or exitshort):
+            self.exchange.entry("Short", False, lot)
+        elif (exitlong or exitshort):
+            self.exchange.close_all()
+
 # サンプル戦略
 class Sample(Bot):
     def __init__(self):
