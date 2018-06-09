7981bcdb83dfb18c8e88e8ec6bd0865c4076e5b7
noda shimpei <noda.sin@gmail.com>
2018-06-09 23:40:00 +0900

    Rename bot

--- restart.sh
+++ restart.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 set -e
-cd /usr/local/bin/daikokuten
+cd /usr/local/bin/ebisu
 
 # env
-rm -f /etc/init.d/daikokuten
-ln -s /usr/local/bin/daikokuten/init.d/daikokuten /etc/init.d/daikokuten
+rm -f /etc/init.d/ebisu
+ln -s /usr/local/bin/ebisu/init.d/ebisu /etc/init.d/ebisu
 
 # force pull
 git fetch origin
 git reset --hard origin/master
 
 # restart all service
-service daikokuten restart
+service ebisu restart
